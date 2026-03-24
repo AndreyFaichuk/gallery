42cc6f868b7f7fcd10213d3f97288ceb49b76839
AndreyFaichuk <andreyfaichuk@gmail.com>
2026-03-24 20:45:34 +0200

    refactor: remove Sentry testing code and clean up painting retrieval logic

--- utils/routeHandlers/getPainting.ts
+++ utils/routeHandlers/getPainting.ts
@@ -1,30 +1,15 @@
 import { eq, ilike } from 'drizzle-orm';
 import { paintings } from '../db/schema';
 import { db } from '../db/db';
-import { NextResponse } from 'next/server';
-import * as Sentry from '@sentry/nextjs';
 
 type Options = {
   id: string;
 };
 
 const getPainting = async ({ id }: Options) => {
-  try {
-    throw new Error('Sentry server test error');
-  } catch (error) {
-    Sentry.captureException(error);
+  const result = await db.select().from(paintings).where(eq(paintings.id, id)).limit(1);
 
-    await Sentry.flush(2000);
-
-    return NextResponse.json(
-      { ok: false, message: 'Sentry server test sent!!!!!!!' },
-      { status: 500 },
-    );
-  }
-
-  // const result = await db.select().from(paintings).where(eq(paintings.id, id)).limit(1);
-
-  // return result[0];
+  return result[0];
 };
 
 export default getPainting;
