94d906f3b60b306e02fe88676df150cb931c513a
AndreyFaichuk <andreyfaichuk@gmail.com>
2026-04-03 20:59:46 +0300

    feat: wrap PaintingItemSkeleton in Suspense for improved loading state handling

--- app/components/ui/skeleton.tsx
+++ app/components/ui/skeleton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import type { FC } from 'react';
+import { Suspense, type FC } from 'react';
 import { cn } from '@/app/lib/utils';
 import { PAINTING_ITEM_VARIANT, type PaintingItemVariantT } from '@/types/painting-types';
 import { PaintingsFilterBar } from '../PaintingsFilterBar';
@@ -36,22 +36,24 @@ export const PaintingItemSkeleton: FC<PaintingItemSkeletonProps> = ({
   const styles = PAINTING_ITEM_SKELETON_STYLES[variant];
 
   return (
-    <PaintingsFilterBar filters={[]} totalCount={0}>
-      <div
-        className={cn(
-          'w-full grid gap-20 justify-items-center max-w-[90%] mx-auto',
-          styles.gridClassName,
-        )}
-      >
-        {Array.from({ length: 8 }).map((_, i) => (
-          // biome-ignore lint/suspicious/noArrayIndexKey: we won't add/delete/move new elements here, so index as a key is ok here
-          <div key={i} className={cn('w-full', styles.rootClassName)}>
-            <div className="aspect-[3/4] w-full rounded bg-neutral-200 animate-pulse" />
-            <div className="mt-4 h-5 w-2/3 rounded bg-neutral-200 animate-pulse" />
-            <div className="mt-2 h-4 w-1/3 rounded bg-neutral-200 animate-pulse" />
-          </div>
-        ))}
-      </div>
-    </PaintingsFilterBar>
+    <Suspense>
+      <PaintingsFilterBar filters={[]} totalCount={0}>
+        <div
+          className={cn(
+            'w-full grid gap-20 justify-items-center max-w-[90%] mx-auto',
+            styles.gridClassName,
+          )}
+        >
+          {Array.from({ length: 8 }).map((_, i) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: we won't add/delete/move new elements here, so index as a key is ok here
+            <div key={i} className={cn('w-full', styles.rootClassName)}>
+              <div className="aspect-[3/4] w-full rounded bg-neutral-200 animate-pulse" />
+              <div className="mt-4 h-5 w-2/3 rounded bg-neutral-200 animate-pulse" />
+              <div className="mt-2 h-4 w-1/3 rounded bg-neutral-200 animate-pulse" />
+            </div>
+          ))}
+        </div>
+      </PaintingsFilterBar>
+    </Suspense>
   );
 };
