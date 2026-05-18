3d81c9f1bf2962b880f7d6ce37f7a104ac4436eb
AndreyFaichuk <andreyfaichuk@gmail.com>
2026-05-18 21:12:06 +0300

    refactor: update Separator component visibility for responsive design

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ const RootLayout: FC<Props> = ({ children }) => {
             <Header />
           </div>
 
-          <Separator className="text-gray-300 mt-1" />
+          <Separator className="text-gray-300 mt-1 hidden xs:block" />
 
           <div className="px-4 sm:px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-60">
             <main className="mt-16 mb-4 flex flex-col gap-4">{children}</main>
