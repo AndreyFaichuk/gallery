5dbdf212f00646bb59bfbd76db8018be255376bb
AndreyFaichuk <andreyfaichuk@gmail.com>
2026-07-02 14:48:02 +0300

    fix: update getAllCollections to use SQL query for fetching collections

--- utils/db/db.ts
+++ utils/db/db.ts
@@ -3,5 +3,3 @@ import { drizzle } from 'drizzle-orm/neon-serverless';
 export const db = drizzle(process.env.DATABASE_URL || '');
 
 export type TransactionType = Parameters<Parameters<(typeof db)['transaction']>[0]>[0];
-
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
--- utils/route-handlers/get-all-collections.ts
+++ utils/route-handlers/get-all-collections.ts
@@ -1,9 +1,10 @@
+import { sql } from 'drizzle-orm';
 import { db } from '../db/db';
 import { collections } from '../db/schema';
 
 export const getAllCollections = async () => {
   try {
-    return await db.select().from(collections);
+    return db.execute(sql`SELECT * FROM collections`);
   } catch (err) {
     console.error(err);
     throw err;
