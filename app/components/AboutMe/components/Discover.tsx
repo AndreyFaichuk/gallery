544bda79d235b33307d0ee785fc7780874e3c315
AndreyFaichuk <andreyfaichuk@gmail.com>
2026-08-08 15:13:23 +0300

    add link to gallery page

--- app/components/AboutMe/components/Discover.tsx
+++ app/components/AboutMe/components/Discover.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { NAVIGATION_MENU_OPTIONS } from '@/constants';
 import { MoveRight } from 'lucide-react';
+import Link from 'next/link';
 
 export const Discover = () => {
   return (
@@ -8,14 +10,19 @@ export const Discover = () => {
       <h2 className="text-xs uppercase tracking-widest text-stone-500 sm:text-md">
         step into the world behind the paintings
       </h2>
-      <h1 className="text-2xl uppercase tracking-widest text-stone-500 sm:text-4xl">
-        discover the gallery
-      </h1>
+      <Link
+        href={NAVIGATION_MENU_OPTIONS.GALLERY.link}
+        className="flex flex-col gap-2 justify-center items-center"
+      >
+        <h1 className="text-2xl uppercase tracking-widest text-stone-500 sm:text-4xl">
+          discover the gallery
+        </h1>
 
-      <div className="flex gap-2 w-full justify-center items-center">
-        <div className="h-px max-w-24 flex-1 bg-black" />
-        <MoveRight className="size-4 text-black" />
-      </div>
+        <div className="flex w-full justify-center items-center gap-2">
+          <div className="h-px max-w-24 flex-1 bg-black" />
+          <MoveRight className="size-4 text-black" />
+        </div>
+      </Link>
     </footer>
   );
 };
