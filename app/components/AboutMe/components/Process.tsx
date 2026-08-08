be25aaf21ac480596eb12538981cb83d462b04e4
AndreyFaichuk <andreyfaichuk@gmail.com>
2026-08-08 15:18:46 +0300

    feat: update descriptive text in Process component

--- app/components/AboutMe/components/Process.tsx
+++ app/components/AboutMe/components/Process.tsx
@@ -52,8 +52,8 @@ export const Process = () => {
           />
 
           <p>
-            A glimpse into my world and the living process behind each work, from the first spark of
-            inspiration.
+            This is an another variant of the text glimpse into my world and the living process
+            behind each work, from the first spark of inspiration. Please, use this one
           </p>
         </div>
         <div className="flex flex-col gap-4">
